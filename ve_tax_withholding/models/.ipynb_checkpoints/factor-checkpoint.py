b55235ed988c7f8764671d6b3711c78b1256ff88
2021*wr*payall <willrr.payall.2021@gmail.com>
2022-02-02 18:10:04 +0000

    [FIX] error in the base._inherits

--- ve_tax_withholding/models/.ipynb_checkpoints/factor-checkpoint.py
+++ ve_tax_withholding/models/.ipynb_checkpoints/factor-checkpoint.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 #factor para calcular la retencion ISLR
 class factor(models.TransientModel): 
     
-    _inherits = 'res.config.settings'
+    _inherit = 'res.config.settings'
     
     factor = fields.Float(
-    string = 'Factor Fiscal', store=True,  )
+    string = 'Factor Fiscal', store=True  )
