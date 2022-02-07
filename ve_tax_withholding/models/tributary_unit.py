4ecbe13416ec539d656fab7f5fb7f03dbc39af3e
willberOdoo <willrr.payall.2021@gmail.com>
2022-02-07 19:14:59 +0000

    [FIX] error in the views of tributary uint

--- ve_tax_withholding/models/tributary_unit.py
+++ ve_tax_withholding/models/tributary_unit.py
@@ -7,12 +7,12 @@ class TributaryUnit(models.Model):
     _name = 'tax.tributary_unit'
     _description = 'model for tributary  unit'
     
-    unit = fields.Float(string='Unidad Tributaria', required = True, store= True)
+    unit = fields.Float(string='Valor de la Unidad Tributaria', required = True, store= True)
     factor = fields.Float(string='Factor Fiscal', required = True, store= True)
     minimum = fields.Float(string='Minimo', default= lambda self: self._compute_minimum() , store= True)
     
     gaceta = fields.Char(string='Nro. Gaceta', store= True)
-    gaceta_fecha = fields.Date(string='Fecha de gaceta', store= True)
+    gaceta_fecha = fields.Date(string='Fecha de la Gaceta', store= True)
     
     @api.onchange('unit', 'factor')
     def _compute_minimum(self):
