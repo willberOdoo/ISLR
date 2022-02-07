59a900853d804b5ce04cbecd5c8537a5072044e8
2021*wr*payall <willrr.payall.2021@gmail.com>
2022-02-07 18:13:30 +0000

    [FIX] views tributary unit

--- ve_tax_withholding/models/tributary_unit.py
+++ ve_tax_withholding/models/tributary_unit.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*- 
-
+import datetime 
 from odoo import models, fields, api
+
 
 class TributaryUnit(models.Model):
     _name = 'tax.tributary_unit'
@@ -10,6 +11,8 @@ class TributaryUnit(models.Model):
     factor = fields.Float(string='Factor Fiscal', required = True, store= True)
     minimum = fields.Float(string='Minimo', default= lambda self: self._compute_minimum() , store= True)
     
+    gaceta = fields.Char(string='Nro. Gaceta', store= True)
+    gaceta_fecha = fields.Date(string='Fecha de gaceta', store= True)
     
     @api.onchange('unit', 'factor')
     def _compute_minimum(self):
